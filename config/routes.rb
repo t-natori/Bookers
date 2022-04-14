0384b6f4ede576b9d0a1755eedee96ae74bbb7cb
EC2 Default User <ec2-user@ip-172-31-87-71.ec2.internal>
2022-04-14 07:53:17 +0000

    add_index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
-  get 'books/index'
+  get 'top' => 'homes#top'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
-  get 'books/show'
+  get 'books/:id' => 'books#show'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_13_074623) do
+ActiveRecord::Schema.define(version: 2022_04_14_043202) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
