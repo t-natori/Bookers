ed3f22eef64e43f00e06c7f56d63dc656f9073de
EC2 Default User <ec2-user@ip-172-31-87-71.ec2.internal>
2022-04-16 05:33:17 +0000

    add_varidation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,11 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       redirect_to book_path(book.id)
     else
       @books = Book.all
-      @book = Book.new
       render :index
     end
   end
@@ -29,9 +28,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
   
 
